c4b0ffa289431e66bf707bf129f60c213aa09e18
IshankaDk <idkodippili@gmail.com>
2021-09-01 13:46:38 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,82 +1,76 @@
 import logo from './logo.svg';
-import React from "react"
+import React, { useState, useEffect } from "react"
 import './App.css';
-import { Button, ButtonGroup, Checkbox, FormControlLabel, TextField, makeStyles } from '@material-ui/core';
+// import { Button, ButtonGroup, Checkbox, FormControlLabel, TextField, makeStyles } from '@material-ui/core';
 import SaveIcon from '@material-ui/icons/Save'
 import DeleteIcon from '@material-ui/icons/Delete'
 
-const useStyle = makeStyles({
-  btn: {
-    background: 'linear-gradient(45deg, #FF8E53 ,#FF6BBB)',
-    border: 0,
-    marginBottom: 10,
-    borderRadius: 15,
-    color: 'white',
-    padding: '5px 25px'
-  }
-})
+// const useStyle = makeStyles({
+//   btn: {
+//     background: 'linear-gradient(45deg, #FF8E53 ,#FF6BBB)',
+//     border: 0,
+//     marginBottom: 10,
+//     borderRadius: 15,
+//     color: 'white',
+//     padding: '5px 25px'
+//   }
+// })
 
-function StyledButton() {
-  const styl = useStyle()
-  return <Button className={styl.btn}>STYLED BUTTON TEST</Button>
-}
+// function StyledButton() {
+//   const styl = useStyle()
+//   return <Button className={styl.btn}>STYLED BUTTON TEST</Button>
+// }
 
-function CheckboxExample() {
-  const [checked, setChecked] = React.useState(true)
-  return (
-    <div>
-      <FormControlLabel
-        control={
-          <Checkbox
-            checked={checked}
-            icon={<DeleteIcon />}
-            checkedIcon={<SaveIcon />}
-            onChange={(e) => {
-              setChecked(e.target.checked)
-            }}
-            inputProps={{
-              'aria-label': 'primary checkbox'
-            }}
-          />
-        }
-        label="Testing CheckBox"
-      />
-    </div>
-  )
-}
+// function CheckboxExample() {
+//   const [checked, setChecked] = React.useState(true)
+//   return (
+//     <div>
+//       <FormControlLabel
+//         control={
+//           <Checkbox
+//             checked={checked}
+//             icon={<DeleteIcon />}
+//             checkedIcon={<SaveIcon />}
+//             onChange={(e) => {
+//               setChecked(e.target.checked)
+//             }}
+//             inputProps={{
+//               'aria-label': 'primary checkbox'
+//             }}
+//           />
+//         }
+//         label="Testing CheckBox"
+//       />
+//     </div>
+//   )
+// }
 
 
 function App() {
+
+  // console.log('render');
+  const [resourceType, setResourceType] = useState('posts')
+  const [items,setItems] =useState([])
+
+  useEffect(() => {
+    fetch(`https://jsonplaceholder.typicode.com/${resourceType}`)
+      .then(response => response.json())
+      .then(json => setItems(json))
+    // console.log("on mount");
+  }, [resourceType])
   return (
-    <div className="App">
-      <header className="App-header">
-        <StyledButton/>
-        <TextField
-          variant="outlined"
-          color='secondary'
-          type='email'
-          label=" The email"
-          placeholder="test@test.com"
-        />
-        <CheckboxExample />
-        <ButtonGroup variant="contained" color="primary">
-          <Button
-            startIcon={<SaveIcon />}
-          >
-            Save
-          </Button>
-          <Button
-            startIcon={<DeleteIcon />}
-          >
-            Discard
-          </Button>
-        </ButtonGroup>
+    <>
+      <div>
+        <button onClick={() => setResourceType('posts')}>Posts</button>
+        <button onClick={() => setResourceType('users')}>Users</button>
+        <button onClick={() => setResourceType('comments')}>Comments</button>
 
-        <img src={logo} className="App-logo" alt="logo" />
-
-
-      </header>
-    </div>
+      </div>
+      <h1>{resourceType}</h1>
+      {items.map(item => {
+        return <pre key={item.id}>{JSON.stringify(item)}</pre>
+      })}
+    </>
   );
 }
 
